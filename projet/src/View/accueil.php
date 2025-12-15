b05b41bdf581d155629b2f9561ef3833a45c800a
melanie976 <melanie.mohamed@etu.u-pec.fr>
2025-12-15 15:31:14 +0100

    Create accueil.php

---
+++ projet/src/View/accueil.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Accueil</title>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+        <h1>Bienvenue sur notre site !</h1>
+        
+        <p>
+            Un premier petit paragraphe.
+        </p>
+        
+        <h2>Les enjeux sur le  réchauffement climatique </h2>
+        
+        <p>
+            Un autre paragraphe contenant un lien pour aller
+            sur le site <a href="http://koor.fr">KooR.fr</a>.
+        </p>
+    </body>
+</html>
