bab72dc37895f0ca0f3e65857b404473d67af5df
melanie976 <melanie.mohamed@etu.u-pec.fr>
2025-12-19 11:08:19 +0100

    Update accueil.php

--- projet/src/View/accueil.php
+++ projet/src/View/accueil.php
@@ -11,11 +11,45 @@
            Sur ce site, il est possible de consulter des données océanographiques relatives à la salinité, à la biomasse du phytoplancton et à la température.
         </p>
         
-        <h2>Les enjeux sur le  réchauffement climatique </h2>
+        <h2>L'impact du réchauffement climatique </h2>
         
         <p>
-            Un autre paragraphe contenant un lien pour aller
-            sur le site <a href="http://koor.fr">KooR.fr</a>.
+             <a href="http://koor.fr">KooR.fr</a>.
         </p>
+
+       <!--Test pour les vidéos -->
+        <div class="videos">
+        <iframe 
+        width="560" 
+        height="315" 
+        src="https://www.youtube.com/embed/KCScTgtajhs"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen>
+        </iframe>
+
+        <iframe 
+        width="560" 
+        height="315" 
+        src="https://www.youtube.com/embed/KCScTgtajhs"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen>
+        </iframe>
+
+        <iframe 
+        width="560" 
+        height="315" 
+        src="https://www.youtube.com/embed/KCScTgtajhs"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen>
+        </iframe>
+        </div>
+
+
     </body>
 </html>
