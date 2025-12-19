581217b46d0bd0908acb2255083e62acfe2e4786
Tmyaa <Toimayamoussa@gmail.com>
2025-12-19 11:31:26 +0100

    Create quizz.php

---
+++ projet/src/View/quizz.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+        <h1>QUIZZ</h1>
+        <form><p>Quel est le principal rôle du phytoplancton dans les océans ? </p></form>
+
+            <label><type d’entrée="radio" name="Réponse1"> A. Produire du sel</label>
+            <label><type d’entrée="radio" name="Réponse2"> B. Absorber le CO₂ et produire de l’oxygène</label>
+            <label><type d’entrée="radio" name="Réponse3"> C. Réchauffer l'eau</label>
+            <label><type d’entrée="radio" name="Réponse4"> D. Augmenter la salinité </label>
+        <input type="submit" value="Valider"></form>
+    
+</body>
+</html>
