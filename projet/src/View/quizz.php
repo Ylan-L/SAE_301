0c90ea2db5b15409f23304e5afb6ad791a999017
Tmyaa <Toimayamoussa@gmail.com>
2026-01-05 14:19:40 +0100

    Update quizz.php

--- projet/src/View/quizz.php
+++ projet/src/View/quizz.php
@@ -5,13 +5,56 @@
     </head>
     <body>
         <h1>QUIZZ</h1>
-        <form><p>Quel est le principal rôle du phytoplancton dans les océans ? </p></form>
+        <form><p>Quel est le principal rôle du phytoplancton dans les océans ? </p>
 
-            <label><type d’entrée="radio" name="Réponse1"> A. Produire du sel</label>
-            <label><type d’entrée="radio" name="Réponse2"> B. Absorber le CO₂ et produire de l’oxygène</label>
-            <label><type d’entrée="radio" name="Réponse3"> C. Réchauffer l'eau</label>
-            <label><type d’entrée="radio" name="Réponse4"> D. Augmenter la salinité </label>
+            <label><type d’entrée="radio" name="Réponse1">  Produire du sel</label>
+            <label><type d’entrée="radio" name="Réponse2">  Absorber le CO₂ et produire de l’oxygène</label>
+            <label><type d’entrée="radio" name="Réponse3">  Réchauffer l'eau</label>
+            <label><type d’entrée="radio" name="Réponse4">  Augmenter la salinité </label>
         <input type="submit" value="Valider"></form>
+
+    <form><p>Pourquoi le phytoplancton est-il utilisé comme indicateur de l’état écologique des océans ? </p>
+            <label><type d’entrée="radio" name="Réponse1">  Il est visible à l’œil nu</label>
+            <label><type d’entrée="radio" name="Réponse2">  Il réagit rapidement aux changements environnementaux</label>
+            <label><type d’entrée="radio" name="Réponse3">  Il vit uniquement en Méditerranée</label>
+            <label><type d’entrée="radio" name="Réponse4">  Il augmente la température de l’eau</label>
+        <input type="submit" value="Valider"></form>
+
+    <form><p>Entre l’océan Atlantique et la mer Méditerranée française, où la concentration en phytoplancton est-elle généralement la plus élevée ?</p>
+            <label><type d’entrée="radio" name="Réponse1">  En Méditerranée</label>
+            <label><type d’entrée="radio" name="Réponse2">  Dans l’Atlantique</label>
+            <label><type d’entrée="radio" name="Réponse3">  Identique dans les deux</label>
+            <label><type d’entrée="radio" name="Réponse4">  Uniquement en été dans les deux </label>
+        <input type="submit" value="Valider"></form>
+
+    <form><p>Quelle est la température moyenne de surface la plus élevée ? </p>
+            <label><type d’entrée="radio" name="Réponse1">  Atlantique français</label>
+            <label><type d’entrée="radio" name="Réponse2">  Méditerranée française</label>
+            <label><type d’entrée="radio" name="Réponse3">  Identique toute l’année</label>
+            <label><type d’entrée="radio" name="Réponse4">  Plus élevée en hiver dans l’Atlantique</label>
+        <input type="submit" value="Valider"></form>
+
+    <form><p>La salinité est :</p>
+            <label><type d’entrée="radio" name="Réponse1">  La quantité de phytoplancton</label>
+            <label><type d’entrée="radio" name="Réponse2">  La température de l’eau</label>
+            <label><type d’entrée="radio" name="Réponse3">  La concentration de sels dissous dans l’eau</label>
+            <label><type d’entrée="radio" name="Réponse4">  La profondeur de l’océan </label>
+        <input type="submit" value="Valider"></form>
+
+    <form><p>Dans quelle zone la salinité est-elle la plus élevée ?</p>
+            <label><type d’entrée="radio" name="Réponse1">  Atlantique français</label>
+            <label><type d’entrée="radio" name="Réponse2">  Méditerranée française</label>
+            <label><type d’entrée="radio" name="Réponse3">  Identique dans les deux</label>
+            <label><type d’entrée="radio" name="Réponse4">  Plus élevée près des pôles</label>
+        <input type="submit" value="Valider"></form>
+
+    <form><p>Quel lien existe entre température et phytoplancton ?</p>
+            <label><type d’entrée="radio" name="Réponse1">  Plus l’eau est chaude, plus il y a toujours de phytoplancton</label>
+            <label><type d’entrée="radio" name="Réponse2">  Le phytoplancton disparaît dans l’eau chaude</label>
+            <label><type d’entrée="radio" name="Réponse3">  La température influence la croissance du phytoplancton</label>
+            <label><type d’entrée="radio" name="Réponse4">  Il n’y a aucun lien </label>
+        <input type="submit" value="Valider"></form>
+    
     
 </body>
 </html>
