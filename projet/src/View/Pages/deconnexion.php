fa619be7b0e8825dadc7217566f43d8533284621
melanie976 <melanie.mohamed@etu.u-pec.fr>
2026-01-06 16:08:50 +0100

    ajout de fichiers

---
+++ projet/src/View/Pages/deconnexion.php
@@ -0,0 +1,14 @@
+<div>
+    <h2>Déconnexion</h2>
+    <p>Voulez-vous vraiment quitter votre session ?</p>
+
+    <div>
+        <button onclick="window.location.href='FrontController.php?action=deconnexion'">
+            Oui, me déconnecter
+        </button>
+        
+        <a href="FrontController.php?action=dashboard">
+            Non, rester
+        </a>
+    </div>
+</div>
