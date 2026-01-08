2444ae4059e344c05d47db6515447872cbe9c819
melanie976 <melanie.mohamed@etu.u-pec.fr>
2026-01-08 21:58:14 +0100

    modif barre de nav
    Enlever inscription et connexion lorsque l'utilisateur est connectée

--- projet/src/View/view.php
+++ projet/src/View/view.php
@@ -11,19 +11,23 @@
     <nav>
         <ul>
             <li><a href="frontController.php?action=accueil">Accueil</a></li>
+
+            <?php if (empty($_SESSION['user_id'])): ?>
+            <!-- Utilisateur NON connecté -->
             <li><a href="frontController.php?action=connexion">Connexion</a></li>
             <li><a href="frontController.php?action=inscription">Inscription</a></li>
-            
-            <?php if (!empty($_SESSION['user_id'])): ?>
-                <li><a href="frontController.php?action=quizz">Quizz</a></li>
-                <li><a href="frontController.php?action=dashboard">Dashboard</a></li>
-                <li><a href="frontController.php?action=profil">Mon Profil</a></li>
-                <li><a href="frontController.php?action=deconnexion">Quitter</a></li>
-            <?php endif; ?>
-            
-            <li><a href="frontController.php?action=graphique">Graphiques</a></li>
-            <li><a href="frontController.php?action=contact_propos">Contact</a></li>
-        </ul>
+            <?php else: ?>
+            <!-- Utilisateur connecté -->
+            <li><a href="frontController.php?action=quizz">Quizz</a></li>
+            <li><a href="frontController.php?action=dashboard">Dashboard</a></li>
+            <li><a href="frontController.php?action=profil">Mon Profil</a></li>
+            <li><a href="frontController.php?action=deconnexion">Quitter</a></li>
+    <?php endif; ?>
+
+    <li><a href="frontController.php?action=graphique">Graphiques</a></li>
+    <li><a href="frontController.php?action=contact_propos">Contact</a></li>
+</ul>
+
     </nav>
 </header>
     <main>
