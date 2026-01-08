ea19ecd9c10bb459b8745d5f577a81f38e8877ce
melanie976 <melanie.mohamed@etu.u-pec.fr>
2026-01-08 11:40:29 +0100

    Update view.php

--- projet/src/View/view.php
+++ projet/src/View/view.php
@@ -14,13 +14,15 @@
                     <li><a href="frontController.php?action=accueil">Accueil</a></li>
                     <li><a href="frontController.php?action=connexion">Connexion</a></li>
                     <li><a href="frontController.php?action=inscription">Inscription</a></li>
+                    <?php if (!empty($_SESSION['user_id'])): ?>
+                        <li><a href="frontController.php?action=deconnexion">Quitter</a></li>
+                    <?php endif; ?>
                     <li><a href="frontController.php?action=quizz">Quizz</a></li>
                     <li><a href="frontController.php?action=graphique">Graphiques</a></li>
                     <!-- Ajout de sections quand l'utilisateur est connecté-->
                     <?php if (!empty($_SESSION['user_id'])): ?>
                         <li><a href="frontController.php?action=dashboard">Dashboard</a></li>
                         <li><a href="frontController.php?action=profil">Mon Profil</a></li>
-                        <li><a href="frontController.php?action=deconnexion">Quitter</a></li>
                     <?php endif; ?>
                     <li><a href="frontController.php?action=contact_propos">Contact</a></li>
                 
