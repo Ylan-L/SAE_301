a63187b0348e75776568272bf250b3fb047bcdbd
melanie976 <melanie.mohamed@etu.u-pec.fr>
2026-02-03 01:02:30 +0100

    changement de placement dans la nav barre
    j'ai juste mis dashboard avant quizz parce que c'est plus cohérent ?

--- projet/src/View/view.php
+++ projet/src/View/view.php
@@ -20,8 +20,8 @@
 
             <?php else: ?>
             <!-- Utilisateur connecté -->
-            <li><a href="frontController.php?action=quizz">Quizz</a></li>
             <li><a href="frontController.php?action=dashboard">Dashboard</a></li>
+            <li><a href="frontController.php?action=quizz">Quizz</a></li>
             <li><a class="spec" href="frontController.php?action=deconnexion">Quitter</a></li>
              <li><a href="frontController.php?action=bilan_carbone">Bilan Carbone</a></li>
         <?php endif; ?>
